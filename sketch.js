5657514d4e99a5f543594d9024997659c7c62dd9
Mina Bavosi <minabavo@hotmail.it>
2021-10-11 12:27:54 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,26 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  angleMode(DEGREES);
+  background("#000000");
+
+  frameRate(120);
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  fill("black");
+
+  let myColor = lerpColor(color("#ffffff"), color("#00ff00"), frameCount / 800);
+  stroke(myColor);
+
+  strokeWeight(frameCount % 2);
+  translate(width / 2, height / 2);
+  rotate(frameCount * 23);
+  smooth();
+  line(0, 0, 0, frameCount / 4);
+  circle(0, frameCount / 2, 25);
+
+  colorMode(HSB, 50);
+  c = color(0, 0, 0);
+
+  loop();
 }
